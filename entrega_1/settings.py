c87f68e2f46e61e14bb714f44c6c6d59466842a2
sarabuch <106285051+sarabuch@users.noreply.github.com>
2022-06-29 23:06:18 -0300

    Pruebas

--- entrega_1/settings.py
+++ entrega_1/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+#STATICFILES_DIRS = [os.path.join (BASE_DIR, 'entega_1/static/')]
 
 MEDIA_URL = '/media/'
 
